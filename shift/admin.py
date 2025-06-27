0d13c323e68b209f57c52ea6897244858184e59a
tsk-brc <tosshy_1219@outlook.com>
2025-06-27 23:48:44 +0900

    add files

--- shift/admin.py
+++ shift/admin.py
@@ -8,6 +8,8 @@ admin.site.register(ShiftType)
 admin.site.register(Shift)
 
 # 管理画面の上部に「シフト表」リンクを追加
-admin.site.site_header = 'シフト管理'
-admin.site.site_title = 'シフト管理'
-admin.site.index_title = format_html('<a href="{}" target="_blank">シフト表を見る</a>', reverse('shift_table'))
+admin.site.site_header = "シフト管理"
+admin.site.site_title = "シフト管理"
+admin.site.index_title = format_html(
+    '<a href="{}" target="_blank">シフト表を見る</a>', reverse("shift_table")
+)
