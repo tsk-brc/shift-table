2bfe369c88f8e600e7b3546f8bc40f32fea9467a
tsk-brc <tosshy_1219@outlook.com>
2025-06-27 23:54:51 +0900

    change to Japanese

--- shift/models.py
+++ shift/models.py
@@ -3,24 +3,34 @@ from django.db import models
 # Create your models here.
 
 class Employee(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField('氏名', max_length=100)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = '従業員'
+        verbose_name_plural = '従業員'
 
 class ShiftType(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField('シフト種別名', max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'シフト種別'
+        verbose_name_plural = 'シフト種別'
+
 class Shift(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    date = models.DateField()
-    shift_type = models.ForeignKey(ShiftType, on_delete=models.PROTECT)
+    employee = models.ForeignKey(Employee, verbose_name='従業員', on_delete=models.CASCADE)
+    date = models.DateField('日付')
+    shift_type = models.ForeignKey(ShiftType, verbose_name='シフト種別', on_delete=models.PROTECT)
 
     class Meta:
         unique_together = ('employee', 'date')
+        verbose_name = 'シフト'
+        verbose_name_plural = 'シフト'
 
     def __str__(self):
         return f"{self.date} {self.employee.name} {self.shift_type.name}"
