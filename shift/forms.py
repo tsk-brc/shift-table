6453533c5988df970bdcb8002414505ed4d0f48f
tsk-brc <tosshy_1219@outlook.com>
2025-06-28 11:39:40 +0900

    add bulk work table

--- shift/forms.py
+++ shift/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Shift, ShiftType, Employee
+from datetime import date
 
 
 class ShiftForm(forms.ModelForm):
@@ -49,3 +50,40 @@ class ShiftForm(forms.ModelForm):
     def get_warnings(self):
         """警告メッセージを取得"""
         return getattr(self, "_warnings", {})
+
+class AutoShiftForm(forms.Form):
+    CREATION_MODE_CHOICES = [
+        ('overwrite', '既存のシフトを上書きして1から作成'),
+        ('fill_gaps', '既存のシフトを保持して空欄の日付に登録'),
+    ]
+    
+    year = forms.IntegerField(
+        label='年',
+        min_value=1900,
+        max_value=2099,
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
+    
+    month = forms.IntegerField(
+        label='月',
+        min_value=1,
+        max_value=12,
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
+    
+    creation_mode = forms.ChoiceField(
+        label='作成モード',
+        choices=CREATION_MODE_CHOICES,
+        widget=forms.RadioSelect(attrs={'class': 'form-control'}),
+        initial='fill_gaps'
+    )
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # デフォルト値を現在の年月に設定
+        today = date.today()
+        if not self.initial:
+            self.initial = {
+                'year': today.year,
+                'month': today.month,
+            }
